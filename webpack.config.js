1ff07091d605fac5f8067cd0b19714491abc144e
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-20 22:58:08 +0700

    index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 var config = {
   context: __dirname + "/app",
   entry: "./main.js",
-
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ],
+  },
   output: {
     filename: "bundle.js",
     path: __dirname + "/dist",
